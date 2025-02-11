6487ba511cfac869757e2c5ec23401c15031f26a
Ardi ansyah <aa9717797@gmail.com>
2025-02-11 16:36:20 +0700

    random string

--- function_public/object.py
+++ function_public/object.py
@@ -1,6 +1,12 @@
-class input_element():
-    usernameInput = "Software Quality Assurance"
-    userEmailInput = "Softwarequalityassurance@yopmail.com"
+class input_element:
+    @staticmethod
+    def id_generator(length=4, chars=string.ascii_letters + string.digits):
+        return ''.join(random.choice(chars) for _ in range(length))
+
+    random_string = id_generator()  # Properly calling static method
+
+    usernameInput = "Software Quality Assurance " + random_string
+    userEmailInput = f"Softwarequalityassurance_{random_string}@yopmail.com"
     currentAddressInput = "Bandung Indigo Hub"
-    permanentAddressInput = "Bandung Lautan API"
+    permanentAddressInput = "Bandung Lautan API" 
     
